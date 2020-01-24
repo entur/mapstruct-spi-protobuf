f1c05abec614af72e211b1803f938d414f800a23
circleci <circleci@entur.no>
2020-01-24 10:47:25 +0000

    Update for next development version

--- spi-impl/src/main/java/no/entur/mapstruct/example/protobuf/ProtobufAccessorNamingStrategy.java
+++ spi-impl/src/main/java/no/entur/mapstruct/example/protobuf/ProtobufAccessorNamingStrategy.java
@@ -1,5 +1,25 @@
 package no.entur.mapstruct.example.protobuf;
 
+/*-
+ * #%L
+ * protobuf-spi-impl
+ * %%
+ * Copyright (C) 2018 - 2020 Entur AS and original authors
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import org.mapstruct.ap.internal.util.Nouns;
 import org.mapstruct.ap.spi.DefaultAccessorNamingStrategy;
 import org.mapstruct.ap.spi.MapStructProcessingEnvironment;
--- usage/src/main/java/no/entur/mapstruct/example/mapper/UserMapper.java
+++ usage/src/main/java/no/entur/mapstruct/example/mapper/UserMapper.java
@@ -18,6 +18,26 @@
  */
 package no.entur.mapstruct.example.mapper;
 
+/*-
+ * #%L
+ * protobuf-usage
+ * %%
+ * Copyright (C) 2018 - 2020 Entur AS and original authors
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import no.entur.mapstruct.example.protobuf.Department;
 import no.entur.mapstruct.example.protobuf.MultiNumber;
 import no.entur.mapstruct.example.protobuf.Permission;
