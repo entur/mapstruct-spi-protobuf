1d871865a5410b0a40c2bf757f2a41aa4d13e897
ro0sterjam <khkwang@gmail.com>
2021-10-28 10:25:13 -0400

    allow for specifying different overrides for different packages

--- README.md
+++ README.md
@@ -21,7 +21,11 @@ which require manual mapping.
 Implements ```EnumMappingStrategy``` and provides complete enum constant mappings if you follow Googles style guide for
 enums https://developers.google.com/protocol-buffers/docs/style#enums
 
-If needed you can specify a different postfix for the 0 value enum by passing in `mapstructSpi.enumPostfix` as a compilerArg.
+If needed you can specify a different postfix for the 0 value enum by passing in `mapstructSpi.enumPostfixOverrides` as 
+a compilerArg in the format of:
+
+`-AmapstructSpi.enumPostfixOverrides=com.package.root.a=POSTFIX_1,com.package.root.b=POSTFIX_2,`
+
 Otherwise, this will default to `UNSPECIFIED` as per the Google style guide.
 
 ```xml
@@ -37,7 +41,7 @@ Otherwise, this will default to `UNSPECIFIED` as per the Google style guide.
             </path>
         </annotationProcessorPaths>
         <compilerArgs>
-            <arg>-AmapstructSpi.enumPostfix=UNSPECIFIED</arg>
+            <arg>-AmapstructSpi.enumPostfixOverrides=com.company.name=INVALID</arg>
         </compilerArgs>
     </configuration>
 </plugin>
---
+++ usage/src/main/java/no/entur/mapstruct/example/EnumPostfixOverrideMapper.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright 2012-2017 Gunnar Morling (http://www.gunnarmorling.de/)
+ * and/or other contributors as indicated by the @authors tag. See the
+ * copyright.txt file in the distribution for a full listing of all
+ * contributors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.entur.mapstruct.example;
+
+/*-
+ * #%L
+ * protobuf-usage
+ * %%
+ * Copyright (C) 2019 - 2020 Entur
+ * %%
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be
+ * approved by the European Commission - subsequent versions of the
+ * EUPL (the "Licence");
+ * 
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ * 
+ * http://ec.europa.eu/idabc/eupl5
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ * #L%
+ */
+
+import no.entur.mapstruct.example.EnumPostfixOverrideProtos.EnumPostfixOverrideValuesDTO;
+import no.entur.mapstruct.spi.protobuf.EnumPostfixOverrideValues;
+import no.entur.mapstruct.spi.protobuf.User;
+import org.mapstruct.CollectionMappingStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.NullValueCheckStrategy;
+import org.mapstruct.ReportingPolicy;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(collectionMappingStrategy = CollectionMappingStrategy.ADDER_PREFERRED, nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS, unmappedSourcePolicy = ReportingPolicy.ERROR, unmappedTargetPolicy = ReportingPolicy.ERROR)
+public interface EnumPostfixOverrideMapper {
+
+	EnumPostfixOverrideMapper INSTANCE = Mappers.getMapper(EnumPostfixOverrideMapper.class);
+
+	EnumPostfixOverrideValuesDTO map(EnumPostfixOverrideValues user);
+
+	EnumPostfixOverrideValues map(EnumPostfixOverrideValuesDTO userDTO);
+
+}
